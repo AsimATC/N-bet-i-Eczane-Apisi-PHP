ea96f158f5e3d0dca451fd949c8f9d19c4966c1f
AsimAtıcı <104257340+AsimATC@users.noreply.github.com>
2024-04-14 21:10:09 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.collectapi.com/health/dutyPharmacy?il=Nigde",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "authorization: apikey 0iT8yXDbBKwVsmd2XGWzd6:7qCyU81GDUwdKMQ2A9iedp",
+    "content-type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  //echo $response;
+}
+
+$pharmacy = json_decode($response, true);
+
+
+$harmacy_all = $pharmacy['result'];
+
+foreach ($harmacy_all as $pharmacys) {
+  echo $pharmacys['name'] . " - " . $pharmacys['dist'] . " - " .  $pharmacys['address'] . " - " .  $pharmacys['phone'] . " - " .  $pharmacys['loc'] . "<hr>";
+}
